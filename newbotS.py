4e9eb3c93504a940456265ff6da5073fd75fde20
e26174222 <31219578+e26174222@users.noreply.github.com>
2017-10-30 08:38:08 -0500

    Add files via upload

--- newbotS.py
+++ newbotS.py
@@ -1740,14 +1740,11 @@ def bot(op):
                     OWN = "u7d8710559bda136ae7030477f83069df"
                     if op.param2 in OWN:
                         return
-                    if op.param2 in admin:
-                        return
+                    else:
                         random.choice(KAC).kickoutFromGroup(op.param1,[op.param2])
                         wait["blacklist"][op.param2] = True
                         f=codecs.open('st2__b.json','w','utf-8')
                         json.dump(wait["blacklist"], f, sort_keys=True, indent=4,ensure_ascii=False)
-                    else:
-                        pass
    
         if op.type == 19:
             try:
