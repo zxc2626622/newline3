163900aa84bec935d15e32674622bc9fe457e102
e26174222 <31219578+e26174222@users.noreply.github.com>
2017-11-11 01:07:02 +0800

    Add files via upload

--- newbotCheck2.py
+++ newbotCheck2.py
@@ -71,8 +71,8 @@ def bot(op):
                 if wait["autoJoin"] == True:
                     cl.acceptGroupInvitation(op.param1)
                     cl.sendText(op.param1,str(wait["message"]))
-                    op.param1.contentMetadata = {'mid': 'u219cfb34db6d3dc96a35023a74579bc0'}
-                    cl.sendMessage(op.param1)
+                    msg = msg.contentMetadata = {'mid': 'uaa75cafbc718153f1b0e69998c51f4e7'}
+                    cl.sendMessage(msg)
         if op.type == 5:
             if wait["autoAdd"] == True:
                 cl.findAndAddContactsByMid(op.param1)
