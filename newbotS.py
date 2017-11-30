ebcc31ff912a8c886c749db88c40ff79f816d4a4
e26174222 <31219578+e26174222@users.noreply.github.com>
2017-12-01 02:17:42 +0800

    Add files via upload

--- newbotS.py
+++ newbotS.py
@@ -374,7 +374,7 @@ def bot(op):
             if ("Tchk" in msg.text):
                 myset = set(ticket)
                 for item in myset:
-                    cl.sendText(msg.to,%d "有" %d "張" % (item,mylist.count(item))
+                    cl.sendText(msg.to,"%d 有 %d 張 " % (item,mylist.count(item))
         if op.type == 25:
             msg = op.message
             if ("Tgive " in msg.text):
